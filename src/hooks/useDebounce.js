18147831480549155c07ecc61c2928310f0405d5
Daniel Baruch <daniel.b@monday.com>
2022-06-21 00:43:30 +0300

    feat(hooks): add useDebounce

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -1,4 +1,13 @@
+import {useEffect, useState} from "react";
+
 export const useDebounce = (value, delay) => {
   // this hook should get a value and delay and return a debounced value
-  return value;
+  const [debouncedValue, setValue] = useState(value);
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setValue(value);
+    }, delay);
+    return () => clearTimeout(timeoutId);
+  }, [value, delay])
+  return debouncedValue;
 };
