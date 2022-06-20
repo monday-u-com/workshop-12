8a9dd3e9393322c8153d3522ffb2e5595ec12afc
Daniel Baruch <daniel.b@monday.com>
2022-06-21 00:38:08 +0300

    feat(hooks): add useIsMounted

--- src/hooks/useIsMounted.js
+++ src/hooks/useIsMounted.js
@@ -1,8 +1,12 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useRef, useEffect } from "react";
 
 export const useIsMounted = () => {
-  // TODO 2: currently you are in a risk for an exception, if you'll start a search and close the modal the app will crash
-  //         this is because you update the state of unmounted component, implement this hook to avoid this.
-  //         this should return a function that would inidicate if the component is mounted or not
-  return useCallback(() => true, []);
+  let isMountedRef = useRef(false);
+  useEffect(() => {
+    isMountedRef.current = true;
+    return () => {
+      isMountedRef.current = false;
+    }
+  })
+  return useCallback(() => isMountedRef.current, []);
 };
