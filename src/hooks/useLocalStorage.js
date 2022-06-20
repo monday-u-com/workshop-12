3286c1cde520c3b17a1ce78dbcc7e4e7674f645d
Daniel Baruch <daniel.b@monday.com>
2022-06-21 00:51:23 +0300

    feat(hooks): add useLocalStorage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 export const useLocalStorage = (storageKey, fallbackState) => {
-  // TODO 4: After every refresh your favorite pokemomn reset, implement this hook to save it
-  // This should act like useState but get the initial value from local storage and save it there
-  // you can use:
-  // localStorage.getItem
-  // localStorage.setItem
   const [value, setValue] = useState(fallbackState);
 
-  return [value, setValue];
+  useEffect(() => {
+    try {
+      const storedItem = localStorage.getItem(storageKey);
+      if (storedItem) setValue(JSON.parse(storedItem));
+    } catch(e) {
+      console.error(e);
+    }
+  }, [storageKey]);
+
+  const storeValue = useCallback((newValue) => {
+    try {
+      localStorage.setItem(storageKey, JSON.stringify(newValue));
+    } catch (e) {
+      console.error(e)
+    }
+    setValue(newValue);
+  }, [storageKey]);
+
+  return [value, storeValue];
 };
